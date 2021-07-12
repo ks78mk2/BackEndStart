1f9d39156bf9a3ddd1bba51121ce96e63a929190
jskim <jskim@iwsys.co.kr>
2021-07-12 16:49:52 +0900

    test

--- test.js
+++ test.js
@@ -31,7 +31,7 @@ app.get('/hash/:input', function (req, res) {
 
 app.get('/hello', function (req, res) {
 
-    res.status(200).send('hello hi12!')
+    res.status(200).send('hello hi123!')
 
 })
 
